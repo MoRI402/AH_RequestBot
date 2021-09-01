a19e8c7239210bb5f4fa920644f80c67829ef27b
Kakarot <81678105+ItzKakarotto@users.noreply.github.com>
2021-09-01 22:42:11 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,46 +1,16 @@
-import logging
+from pyrogram import Client, filters
+from pyrogram import Client
 
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+app = from pyrogram import Client
 
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+app = Client(
+    "my_bot",
+    bot_token="123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"
 )
 
-logger = logging.getLogger(__name__)
-
-@run_async
-def start(update: Update, context: CallbackContext):    
-    user = update.effective_user
-    update.message.reply("
-What do you want to Request?
-Choose from Give Options Below
-━━━━━━━━━━━━━━━━━━━━━━━
-» Walls - Anime Wallpapers
-» Anime - Anime and Anime Movies
-» Music - Anime Music (OP/ED/OST)
-» Manga - Manga or Manhwas
-» Themes - Custom Made Telegram Themes
-» Stickers - Custom Made Sticker Packs
-» Post - Post Your Art/AMVs/Memes on @Anime_Heaven_Official
-━━━━━━━━━━━━━━━━━━━━━━━
- ")
-
-buttons = [
-    [        
-        InlineKeyboardButton(
-        text="Anime", callback_data="anime_"
-        ),
-    ],    
-]
+@app.on_message(filters.command('start'))
+async def main(client, message):
+    await message.reply_text()
 
 
-@run_async
-def bot_about_callback(update, context):
-    query = update.callback_query
-    if query.data == "anime_":
-        animee = client.ask(text= "Tell the Name of Anime You Want to Request".)
-    
-
- 
+app.run()
